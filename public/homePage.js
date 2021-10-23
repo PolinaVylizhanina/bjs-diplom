24fed6f9897bc7a1ce3025e078f8eb54a7772b2e
Polina Vylizhanina <vylizhaninaps@gmail.com>
2021-10-23 12:46:36 +0500

    added money transaction and favorite list

--- public/homePage.js
+++ public/homePage.js
@@ -19,11 +19,91 @@ ApiConnector.current(callback => {
 //курсы валют
 const rates = new RatesBoard();
 
-ApiConnector.getStocks(callback => {
+
+function getStock() {
+
+    ApiConnector.getStocks(callback => {
     if(callback.success === true) {
         rates.clearTable();
         rates.fillTable(callback.data);
-    }
-});
+        }
+    });
+}
 
-setInterval(() => {ApiConnector.getStocks}, 60000);
+setInterval(getStock(), 60000);
+
+// операции с деньгами
+
+const moneyTransaction = new MoneyManager();
+
+moneyTransaction.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, callback => {
+        if(callback.success === true) {
+            ProfileWidget.showProfile(callback.data);
+            moneyTransaction.setMessage(callback.success, 'Кошелек успешно пополнен');
+        } else {
+            moneyTransaction.setMessage(callback.success, callback.error);
+        }
+    })
+}
+
+moneyTransaction.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, callback => {
+        if(callback.success === true) {
+            ProfileWidget.showProfile(callback.data);
+            moneyTransaction.setMessage(callback.success, 'Валюта успешно сконвертирована');
+        } else {
+            moneyTransaction.setMessage(callback.success, callback.error);
+        }
+    })
+}
+
+moneyTransaction.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, callback => {
+        if(callback.success === true) {
+            ProfileWidget.showProfile(callback.data);
+            moneyTransaction.setMessage(callback.success, 'Перевод осуществлен');
+        } else {
+            moneyTransaction.setMessage(callback.success, callback.error);
+        }
+    })
+}
+
+// работа с избранным
+
+const favoritTab= new FavoritesWidget()
+
+ApiConnector.getFavorites (callback => {
+    if(callback.success === true) {
+        favoritTab.clearTable();
+        favoritTab.fillTable(callback.data);
+        moneyTransaction.updateUsersList(callback.data);
+    }
+})
+
+
+favoritTab.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, callback => {
+        if(callback.success === true) {
+            favoritTab.clearTable();
+            favoritTab.fillTable(callback.data);
+            moneyTransaction.updateUsersList(callback.data);
+            favoritTab.setMessage(callback.success, 'Пользователь успешно добавлен');
+        } else {
+            favoritTab.setMessage(callback.success, callback.error);
+        }
+    })
+}
+
+favoritTab.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, callback => {
+        if(callback.success === true) {
+            favoritTab.clearTable();
+            favoritTab.fillTable(callback.data);
+            moneyTransaction.updateUsersList(callback.data);
+            favoritTab.setMessage(callback.success, 'Пользователь успешно удален');
+        } else {
+            favoritTab.setMessage(callback.success, callback.error);
+        }
+    })
+}
